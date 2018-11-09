ecc63e6f9ec6c4d6399ff5816cf5fc83f71a1c2b
Muhammad Yahya <yahya_gis@hotmail.com>
2018-11-09 17:24:25 +0100

    gulp file updated

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -1,26 +1,30 @@
-var gulp = require('gulp'),
-  watch = require('gulp-watch'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  cssvars = require('postcss-simple-vars'),
-  nested = require('postcss-nested');
+const gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars');
 
-gulp.task('html', function () {
+gulp.task('default', function () {
   console.log('index file is updated!');
 });
 
-gulp.task('styles', function () {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([nested, cssvars, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
+gulp.task('html', () => {
+  console.log('hi');
 });
 
-gulp.task('watch', function () {
-  watch('./app/index.html', function () {
+gulp.task('styles', ()=>{
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, autoprefixer]))
+    .pipe(gulp.dest('./app/dist/styles'));
+
+})
+
+gulp.task('watch', ()=> {
+  watch('./app/index.html', () => {
     gulp.start('html');
   });
 
-  watch('./app/assets/styles/**/*.css', function () {
+  watch('./app/assets/styles/**/*.css', () =>{
     gulp.start('styles');
-  });
-});
+  })
+})
